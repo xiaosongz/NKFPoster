ea5938d63cc9d96081005eb2a85d8822bf2c2a0b
Xiaosong Zhang <xiaosongzhang@XMacmini.ddns.sph.umich.edu>
2019-03-26 16:53:07 -0400

    add como_linegroups test code, need to finish.

---
+++ como_linegroups.R
@@ -0,0 +1,124 @@
+# ploting figures
+
+# load data ---------------------------------------------------------------
+
+source("DataPrep.R")
+
+# Load libraries ----------------------------------------------------------
+
+library(ggplot2)
+library(ggthemes)
+library(ggsci)
+
+# define comos ------------------------------------------------------------
+
+comos <- c("DM",
+           "HTN",
+           "CHF",
+           "CVD",
+           "AKI",
+           #"CVD_HOSP",
+           #"INF_HOSP",
+           "ANEMIA")
+
+# Prepare dataset for plotting --------------------------------------------
+
+
+draw <- data  %>%
+  select(var,year,Overall,starts_with("peds"),starts_with("adult")) %>%
+  mutate(overall_prop = extract_prop(Overall),
+         peds_prop = extract_prop(peds),
+         adult_prop = extract_prop(adult),
+         adultNoCKD_prop = extract_prop(adultNoCKD),
+         adultCKD_prop = extract_prop(adultCKD),
+         pedsNoCKD_prop = extract_prop(pedsNoCKD),
+         pedsCKD_prop = extract_prop(pedsCKD)) %>%
+  select(var,year,ends_with("_prop")) %>%
+  filter(var %in% comos) %>%
+  rename(key = var,
+         value =pedsNoCKD_prop )
+
+p <- ggplot(data = draw,
+            aes(x = year,
+                y = value,
+                group = key,
+                color = key))
+p + geom_line(size = 2)+
+  ggthemes::theme_economist()+
+  ggsci::scale_fill_jama()+
+  ggsci::scale_color_jama()+
+  ylab(label = "Prevlence (%)")+
+  ggtitle("CKD Diagnosis Trends by Patients' Age Group")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+ggsave(height = 6,
+       width = 6*1.6,
+       filename = "plots/CKDTrendByAgegroup.pdf")
+
+# ED visits trend by agegroups ----------------------------------------------
+
+
+draw <- data %>% select(year,var,Overall,peds,adult) %>%
+  filter(var == "ED") %>%
+  mutate(overall_prop = extract_prop(Overall),
+         peds_prop = extract_prop(peds),
+         adult_prop = extract_prop(adult)) %>%
+  select(year, ends_with("_prop")) %>%
+  gather(key = "key",value = "value",
+         overall_prop,peds_prop,adult_prop)
+
+p <- ggplot(data = draw,
+            aes(x = year,
+                y = value,
+                group = key,
+                color = key))
+p + geom_line(size = 2)+
+  ggthemes::theme_economist()+
+#  ggsci::scale_fill_jama()+
+   ggsci::scale_color_jama()+
+  ylab(label = "Eemergency Room Usage (%)")+
+  ggtitle("ED visits trend by age group") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggsave(height = 6,
+       width = 6*1.6,
+       filename = "plots/EDTrendByAgegroup.pdf")
+
+
+# ED visits by Age and CKD status -----------------------------------------
+
+
+draw <- data %>% select(year,var,Overall,peds,adult,
+                        pedsNoCKD,adultNoCKD,pedsCKD,adultCKD) %>%
+  filter(var == "ED") %>%
+  mutate(overall_prop = extract_prop(Overall),
+         peds_prop = extract_prop(peds),
+         adult_prop = extract_prop(adult),
+         adultNoCKD_prop = extract_prop(adultNoCKD),
+         adultCKD_prop = extract_prop(adultCKD),
+         pedsNoCKD_prop = extract_prop(pedsNoCKD),
+         pedsCKD_prop = extract_prop(pedsCKD)
+         ) %>%
+  select(year, ends_with("_prop")) %>%
+  gather(key = "key",value = "value",
+         ends_with("_prop"))
+
+p <- ggplot(data = draw,
+            aes(x = year,
+                y = value,
+                group = key,
+                color = key))
+p + geom_line(size = 2)+
+  ggthemes::theme_economist()+
+  #  ggsci::scale_fill_jama()+
+  ggsci::scale_color_jama()+
+  ylab(label = "Eemergency Room Usage (%)")+
+  ggtitle("ED visits trend by age group") +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = "right")+
+  scale_fill_discrete(name = "Patient group")
+
+ggsave(height = 6,
+       width = 6*1.6,
+       filename = "plots/EDTrendByAge_CKD.pdf")
