0bfb523175dca2841a3c40c98df65f32f0bcf68b
Xiaosong Zhang <zorroxinxsindi@gmail.com>
2019-03-26 00:31:44 -0400

    update data prep code to get clean variable names. remote test code.

--- DataPrep.R
+++ DataPrep.R
@@ -1,17 +1,5 @@
 library(tidyverse)
 library(stringr)
-year = 2013
-m2013 <- read_csv("data/results/2013/MainByAgeCKD.csv")
-m2013 <- m2013 %>% rename(var = "X1",
-                          peds = "(0,21]",
-                          adult = "(21,120]",
-                          pedsNoCKD = "(0,21]:0",
-                          pedsCKD = "(0,21]:1",
-                          adultNoCKD = "(21,120]:0",
-                          adultCKD = "(21,120]:1"
-                        ) %>%
-  mutate(year = year)
-
 dataprep <- function(year) {
   data <- read_csv(paste0("data/results/",year,"/MainByAgeCKD.csv"))
   data <- data %>% rename(var = "X1",
@@ -26,8 +14,8 @@ dataprep <- function(year) {
   return(data)
 }
 
-rm(data)
-rm(out)
+# rm(data)
+# rm(out)
 for (year in seq(2007,2013)) {
   print(year)
   data <- dataprep(year = year)
@@ -48,7 +36,10 @@ for (year in seq(2007,2013)) {
 
 }
 
-var_c <- out %>% mutate(var_a = sub("\\=.*","",x = var)) %>% select(var_a)
+var_c <- out %>% mutate(var_a = sub("\\ =.*","",x = var)) %>% select(var_a)
 
-out <-  out %>% mutate(var_a = sub("\\=.*","",x = var))
+data <-  out %>%
+  mutate(var_a = sub("\\ =.*","",x = var)) %>%
+  rename(var_old = var,
+         var = var_a)
 
