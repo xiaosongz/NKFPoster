cab31e445108c944de53b01995bc974d1e27c3d1
Xiaosong Zhang <zorroxinxsindi@gmail.com>
2019-04-10 23:38:55 -0400

    add county level mapping

--- CKDmap.R
+++ CKDmap.R
@@ -13,20 +13,22 @@ names(prevbyzip) <- names(prevbyzip) %>% tolower()
 library(choroplethrZip)
 library(choroplethr)
 data(df_pop_zip)
-data("zip.map")
+data("county.map")
+data(df_pop_county)
+data(zipcode)
 
 state.name.lower <-  state.name %>% tolower()
 map_by_state <- function(state,yr,como) {
 
-  df_zip <- prevbyzip %>%
+  df_zip <- prevbyzip %>%filter(statecode == "MI" & year == yr) %>%
     mutate(region = zip) %>%
-    right_join(df_pop_zip, by = "region") %>%
+    left_join(df_pop_zip, by = "region") %>%
     rename("pop" = value,
            "target" = !!como) %>%
     mutate(value = target*1000/pop)
 
-  tomap <- df_zip %>% filter(year == yr)
-  zip_choropleth(tomap,
+
+  zip_choropleth(df_zip,
                  state_zoom = state,
                  #county_zoom = "26161",
                  title      = paste0(yr," ",
@@ -40,3 +42,33 @@ map_by_state <- function(state,yr,como) {
   ggsave(paste0(file_name,".pdf"),height = 12, width = 14)
 }
 
+# map by county
+
+
+map_by_cty <- function(state,yr,como) {
+
+  df_cty <- prevbyzip %>%
+    filter(statecode == "MI" & county != "" & year == yr) %>%
+    mutate(region = state*1000+county) %>%
+    rename("target" = !!como) %>%
+    group_by(region) %>%
+    summarise(target= sum(target)) %>%
+    left_join(df_pop_county, by = "region") %>%
+    rename("pop" = value,
+           "target" = target) %>%
+    filter(target >=10) %>%
+    mutate(value = target*1000/pop)
+
+county_choropleth(df_cty,
+                 state_zoom = state,
+                 #county_zoom = "26161",
+                 title      = paste0(yr," ",
+                                     # state,
+                                     paste0(" Michigan State prev ", como," by county")),
+                 legend     = paste0(como, " per 1000 residents"),
+                 num_colors = 8)
+
+  file_name <- paste0("plots/maps/",como,"/",yr,state,"_",como)
+  ggsave(paste0(file_name,".png"),height = 12, width = 14)
+  ggsave(paste0(file_name,".pdf"),height = 12, width = 14)
+}
