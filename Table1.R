9fcce6a1d7a539e60e8a60b1c61c25de1b92b45d
Xiaosong Zhang <zorroxinxsindi@gmail.com>
2019-04-17 00:47:59 -0400

    update table 1 creation, add more columns, now have a seperate peds table 1 with new stratification

--- Table1.R
+++ Table1.R
@@ -1,8 +1,42 @@
 # extract table one from result table
+# load data ---------------------------------------------------------------
 
-demo_vars <- as.character(tab1$var)[c(1:10,60)]
+source("DataPrep.R")
+demo_vars <- c("n",
+               "EL_SEX_CD",
+               "race (%)",
+               "White",
+               "Black",
+               "Native American",
+               "Asian or PI",
+               "Hispanic",
+               "NH PI",
+               "NA.",
+               "age (mean (sd))")
+ # as.character((data %>% filter(year== 2013))$var)[c(1:10,60)]
 
 tab1 <- data %>% filter(year == 2013) %>%
-  select(var,Overall,peds,adult) %>%
+  select(var,Overall,peds,pedsCKD,adult,adultCKD) %>%
     filter(var %in% demo_vars) %>% write_csv(path = "data/results/tab1.csv")
+
+
+
+source("Datapeds.R")
+demo_vars <- c("n",
+               "EL_SEX_CD = M (%)",
+               "race (%)",
+               "White",
+               "Black",
+               "Native American",
+               "Asian or PI",
+               "Hispanic",
+               "NH PI",
+               "NA.",
+               "age (mean (sd))")
+# as.character((data %>% filter(year== 2013))$var)[c(1:10,60)]
+
+tab1 <- data %>% filter(year == 2013) %>%
+  select(var,Overall,PEDCKD,No_PEDCKD) %>%
+  filter(var %in% demo_vars) %>% write_csv(path = "data/results/tab1_peds.csv")
+
 
