19c53bf8d3b0f84f161b16fd76e024470d9efa15
Xiaosong Zhang <xiaosongzhang@XMacmini.ddns.sph.umich.edu>
2019-04-18 16:26:50 -0400

    working changes, plotting formats

--- func_line_groups.R
+++ func_line_groups.R
@@ -97,33 +97,37 @@ linegroups_age_CKD <- function(thevar) {
 # line groups function
 peds_linegroups_byage <- function(thevar) {
 
-  draw <- data %>% select(year,var,Overall,starts_with("age")) %>%
+  draw <- data %>% select(year,var,starts_with("age")) %>%
     filter(var %in% c('n',!!thevar)) %>% #get cells for both denominator n and the varaible we want
-    mutate_at(vars(Overall,starts_with("age")),list(ext_n)) %>% # extract counts(n) from the combined cell text and convert to `double``
-    select(var,year, Overall,matches("\\d$")) %>% # select columns of need
+    mutate_at(vars(starts_with("age")),list(ext_n)) %>% # extract counts(n) from the combined cell text and convert to `double``
+    select(var,year,matches("\\d$")) %>% # select columns of need
     gather(key = "key",value = "value",
-           Overall,starts_with("age")) %>% # create a long format, create key for all vars except "var"
+           starts_with("age")) %>% # create a long format, create key for all vars except "var"
     spread(key = var,value = value) %>% # spread the !!thevar and n into two columns for calculate the %
-    rename(count = !!thevar) %>% # rename the !!thevar column to "count", workaound for "non-numeric argument to binary operator" problem
+    rename(count = !!thevar,
+           Year = "year") %>% # rename the !!thevar column to "count", workaound for "non-numeric argument to binary operator" problem
     mutate(prop = ((count)/n*100)) # create new prop % variable for drawing
 
+ draw$key %>% forcats::as_factor(.,c("Age 0-5", "Age 6-10", "Age 11-13", "Age 14-18", "Age 19-22"))
   p <- ggplot(data = draw,
-              aes(x = year,
+              aes(x = Year,
                   y = prop,
                   group = key,
                   color = key))
   p + geom_line(size = 2)+
     ggthemes::theme_tufte()+
-    ggsci::scale_color_nejm(#labels = c("Adult CKD","Adult No-CKD","Peds CKD","Peds No-CKD")
+    ggsci::scale_color_nejm(#labels = c("Age 0-5", "Age 6-10", "Age 11-13", "Age 14-18", "Age 19-22")
     )+
     ylab(label = paste0(thevar,"( %)"))+
     ggtitle(paste0(thevar," trend by age group")) +
-    theme(plot.title = element_text(hjust = 0.5),
-          legend.position = "right")+
     scale_fill_discrete(name = "Patient group") +
-    scale_x_continuous(breaks = pretty(draw$year))+
-    theme(legend.title=element_blank(),
-          legend.position = "top")
+    scale_x_continuous(breaks = pretty(draw$Year))+
+    scale_y_continuous(limits = c(0,ceiling(max(draw$prop))),breaks = seq(0,ceiling(max(draw$prop)),ceiling(max(draw$prop))/5)) +
+    theme(plot.title = element_blank(),
+          legend.title=element_blank(),
+          legend.position = "top",
+          axis.title = element_text(family = "sans",size = 14),
+          axis.text = element_text(family = "sans", size = 12))
 
   ggsave(height = 6,
          width = 6*1.61,
