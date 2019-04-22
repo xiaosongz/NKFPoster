02f8d6a754f4309f763b0f69d1595587366fae4c
Xiaosong Zhang <zorroxinxsindi@gmail.com>
2019-04-21 21:14:43 -0400

    add manually twicked plot code that use hard coded axis limits which will not work for other plots.

---
+++ manual_plots/ped_grp_bars.R
@@ -0,0 +1,61 @@
+thisyear = 2013
+vars = comos
+
+# make barplot peds ------------------------------------------------------------
+
+plot_data <- data %>% filter(year == thisyear) %>%
+  select(var,Overall,starts_with("peds"),starts_with("adult")) %>%
+  mutate(overall_prop = extract_prop(Overall),
+         peds_prop = extract_prop(peds),
+         adult_prop = extract_prop(adult),
+         adultNoCKD_prop = extract_prop(adultNoCKD),
+         adultCKD_prop = extract_prop(adultCKD),
+         pedsNoCKD_prop = extract_prop(pedsNoCKD),
+         pedsCKD_prop = extract_prop(pedsCKD)) %>%
+  select(var,ends_with("_prop")) %>%
+  filter(var %in% vars)
+
+plotting_df <-
+  plot_data %>%
+  select(var,pedsNoCKD_prop,pedsCKD_prop) %>%
+  gather(key = "group", value = "prop",
+         pedsNoCKD_prop,pedsCKD_prop)
+# create dynamic lower and upper limit based on ploting data
+axis_limit <- max(plotting_df$prop) *1.15;axis_limit
+## find the order
+temp_df <-
+  plotting_df %>%
+  filter(group == "pedsNoCKD_prop") %>%
+  arrange(prop)
+# plot
+p <-
+  plotting_df %>%
+  ggplot(aes(x = var, y = prop, group = group, fill = group)) +
+  geom_bar(stat = "identity",
+           position = position_dodge(),
+           width = 0.7) +
+  coord_flip() +
+  scale_x_discrete(limits = rev(vars)) +
+  # another trick!
+  scale_y_continuous(limits = c(0,20),#set the lower and upper limit for x axis(flipped)
+                     breaks = seq(-100, 100, 5),
+                     labels = abs(seq(-100, 100, 5))) +
+  labs(x = "Comobidity", y = "Proportion (%) of Children", title = paste0(thisyear," Pediatric ",name, " diagnosis by CKD status")) +
+  ggthemes::theme_tufte() +
+  ggsci::scale_fill_nejm(labels = c("CKD","No-CKD")) +
+  theme(plot.title = element_blank(),
+        legend.title=element_blank(),
+        legend.position = "top",
+        #axis.title = element_text(family = "sans",size = 14),
+        text = element_text(family = "sans", size = 14)) +
+  # reverse the order of items in legend
+  guides(fill = guide_legend(reverse = TRUE))
+
+print(p)
+
+ggsave(height = 6,
+       width = 6*1.61,
+       filename = paste0("plots/grp_bars/pdf/",thisyear,"peds",name,"byCKD.pdf"))
+ggsave(height = 6,
+       width = 6*1.61,
+       filename = paste0("plots/grp_bars/png/",thisyear,"peds",name,"byCKD.png"))
--- top_ckd_prev_cty.R
+++ top_ckd_prev_cty.R
@@ -32,3 +32,40 @@ df_cty %>%
          "pedckd_per_1000_pts" = value) %>%
   arrange(desc(pedckd_per_1000_pts)) %>% write_csv("data/results/top_peds_CKD.csv")
 df_cty$target %>% sum
+
+
+data(county.fips)
+
+prevbycty <- read_csv("data/results/prevbycty.csv")
+
+names(prevbycty) <- names(prevbycty) %>% tolower()
+
+
+library(choroplethr)
+data("county.map")
+data(df_pop_county)
+
+state_name_lower <-  state.name %>% tolower()
+
+
+
+df_cty <- prevbycty %>%
+  filter( county != "" & year == yr) %>%
+  mutate(region = 26*1000+county) %>%
+  rename("target" = "ckd",
+         "pts"  = n)  %>%
+  left_join(df_pop_county, by = "region") %>%
+  rename("pop" = value,
+         "target" = target) %>%
+  filter(target >=10) %>%
+  mutate(value = target*1000/pts)
+
+df_cty %>%
+  rename("fips" = region )%>%
+  left_join(county.fips, by = "fips") %>%
+  mutate(county = stringr::str_split(polyname,pattern = ",",simplify = T)[,2]) %>%
+  rename("pedckd" = target,
+         "pedckd_per_1000_pts" = value) %>%
+  arrange(desc(pedckd_per_1000_pts)) %>% write_csv("data/results/top_CKD.csv")
+df_cty$target %>% sum
+
